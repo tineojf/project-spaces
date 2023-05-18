8d8e663cfee7428c76b79e364ba44e459725c61e
tineojf <92743762+tineodev@users.noreply.github.com>
2023-05-18 09:06:08 -0500

    Add corporative

--- assets/index.js
+++ assets/index.js
@@ -1,12 +1,12 @@
 function redirigir() {
   const numero = document.getElementById("numero").value;
   if (numero == 1) {
-    window.location.href = "/project-spaces/src/casa.html" + "?integrantes=" + numero;
+    window.location.href = "src/casa.html" + "?integrantes=" + numero;
   } else if (numero > 1 && numero <= 8) {
-    window.location.href = "/project-spaces/src/estudio.html" + "?integrantes=" + numero;
+    window.location.href = "src/estudio.html" + "?integrantes=" + numero;
   } else if (numero > 8 && numero <= 16) {
-    window.location.href = "/project-spaces/src/mediana.html" + "?integrantes=" + numero;
+    window.location.href = "src/mediana.html" + "?integrantes=" + numero;
   } else {
-    window.location.href = "/project-spaces/src/corporativa.html" + "?integrantes=" + numero;
+    window.location.href = "src/corporativa.html" + "?integrantes=" + numero;
   }
 }
