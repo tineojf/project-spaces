f56f01848970491fd76344d062758e33c339dcff
tineojf <92743762+tineodev@users.noreply.github.com>
2023-05-17 19:47:20 -0500

    Fix home

--- assets/index.js
+++ assets/index.js
@@ -2,9 +2,9 @@ function redirigir() {
   const numero = document.getElementById("numero").value;
   if (numero == 1) {
     window.location.href = "/project-spaces/src/casa.html" + "?integrantes=" + numero;
-  } else if (numero > 1 && numero <= 5) {
+  } else if (numero > 1 && numero <= 8) {
     window.location.href = "/project-spaces/src/estudio.html" + "?integrantes=" + numero;
-  } else if (numero > 5 && numero <= 20) {
+  } else if (numero > 8 && numero <= 16) {
     window.location.href = "/project-spaces/src/mediana.html" + "?integrantes=" + numero;
   } else {
     window.location.href = "/project-spaces/src/corporativa.html" + "?integrantes=" + numero;
